bd0b546ff05ebee13368bfd07cd95d78e089ef5c
Alex <alex@mail.se>
2017-06-28 10:34:50 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Smile</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link href='https://fonts.googleapis.com/css?family=Raleway:400,100,200,500,600,700,300' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div class="wrapper">
+		<header class="container-fluid">
+			<div class="row">
+				<h1 class="logo">Quotes</h1>
+				<nav class="clearfix">
+					<a href="#" class="menu-icon">Menu</a>
+					<ul>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+				</nav>
+			</div> <!-- END .row -->
+		</header> <!-- END .container-fluid -->
+
+		<section class="carousel container-fluid">
+			<div class="row">
+				<img src="images/bear.jpg" alt="Smiling teddy-bear sitting">
+				<div class="carousel-title">
+					<h1>Smile</h1>
+					<p>it costs you nothing</p>
+				</div>
+			</div> <!-- END .row -->
+		</section> <!-- END .container-fluid -->
+
+		<section class="intro-text container-fluid">
+			<div class="row">
+				<h1>The most inclusive <br><span>Free PSD Template</span></h1>
+				<p><span class="divider">Free for commercial and personal use.<br>
+					You can easily edit its contents and code it for yourself or your client!</span></p>
+
+					<!-- Divider -->
+
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget nisi eget tortor iaculis tristique.
+						Aliquam tincidunt posuere tortor hendrerit elementum. Nunc laoreet lorem a sem auctor fringilla. 
+						Pellentesque dignissim accumsan lobortis. 
+						Sed pharetra erat vitae dui imperdiet quis commodo orci dapibus.</p>
+
+					<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+						Fusce at nunc libero. 
+						Nam metus lorem, rhoncus ac mattis ornare, blandit vel risus.</p>
+			</div> <!-- END .row -->			
+		</section> <!-- END .container-fluid -->
+
+		<section class="quote-carousel container-fluid">
+			<div class="row">
+				<blockquote>
+					<p>I love people who make me laugh. I honestly think it’s the thing I like most, to laugh.
+						It cures a multitude of ills. 
+						It's probably the most important thing in a person.</p>
+					<cite>Audrey Hepburn</cite>
+					<ul>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+				</blockquote>
+
+				<figure>
+					<img src="images/pair.jpg" alt="Two persons leaning on wall">
+				</figure>
+			</div> <!-- END .row -->	
+		</section> <!-- END .container-fluid -->
+
+		<section class="quote-gallery container-fluid">
+			<div class="row">
+				<h1 class="divider">Reasons to be happy</h1>
+				<!-- Divider -->
+				<div class="quotes-container">
+					<blockquote>
+						<h2>You are alive</h2>
+						<p>Waking up to see another day is a blessing. 
+							Don't take it for granted. Make it count and be happy that you're alive.</p>
+						<cite>Anonymous</cite>
+					</blockquote>
+
+					<blockquote>
+						<h2>Someone loves you</h2>
+						<p>Someone who really loves you sees what a mess you can be,
+							how moody you can get, how hard you are to handle,
+							but still wants you in their life.</p>
+						<cite>Anonymous</cite>
+					</blockquote>
+
+					<blockquote>
+						<h2>Enjoy the moment</h2>
+						<p>Sometimes you will never know the true value of a 
+							moment until it becomes a memory.</p>
+						<cite>Anonymous</cite>
+					</blockquote>
+
+					<blockquote>
+						<h2>Health is wealth</h2>
+						<p>It is health that is real wealth and not 
+							pieces of gold and silver.</p>
+						<cite>Mahatma Gandhi</cite>
+					</blockquote>
+					<a href="#" class="quote-button">More reasons</a>
+				</div> <!-- END .quotes-container -->	
+			</div> <!-- END .row -->	
+		</section> <!-- END .container-fluid -->
+
+		<section class="love-bg container-fluid">
+			<div class="row">
+				<img src="images/hands.jpg" alt="Holding hands">
+				<blockquote>
+					<p>I love you for all that you are,<br>
+						all that you have been,<br> 
+						and all you're yet to be.
+					</p>
+					
+					<cite>
+						Quote of the day, from <span>Love Quotes</span>
+					</cite>
+				</blockquote>
+			</div> <!-- END .row -->	
+		</section> <!-- END .container-fluid -->
+
+		<section class="prints container-fluid">
+			<div class="row">
+				<div class="print-title">
+					<h1 class="divider">Buy Poster Prints</h1>
+				</div> <!-- END .print-title -->
+				<!-- Divider -->
+				<figure>
+					<img src="images/suit.jpg" alt="Man in suit and tie">
+					<h2 class="main-content">Stay<br> true,<br> stay<br> gentleman</h2>
+					<div class="call-to-action">
+						<p class="left">Buy now</p>
+						<p class="right">Share</p>
+					</div>	
+				</figure>
+
+				<figure>
+					<img src="images/mountain.jpg" alt="Man sitting on a rock">
+					<blockquote>
+						<p>And muse of nature<br>
+							with a poet's eye</p>
+						<cite>Thomas Campbell</cite>
+					</blockquote>
+				</figure>
+
+				<figure class="double left pic-one">
+					<img src="images/coffee.jpg" alt="Coffee with foam like a smile">
+					<h2>make work<br> fun &amp; play</h2>
+				</figure>
+
+				<figure class="double left pic-two">
+					<img src="images/silence.jpg" alt="Man doing the silence-gesture with pointing finger">
+					<h2>Silence is<br> <span>golden</span></h2>
+				</figure>
+				<div class="container-btn">
+					<a href="#" class="browse-button">Browse posters</a>
+				</div> <!-- END .container-btn -->
+			</div> <!-- END .row -->
+		</section> <!-- END .container-fluid -->
+
+		<footer class="container-fluid">
+			<div class="row">
+				<nav class="footer-nav">
+					<h1>Browse</h1>
+					<ul>
+						<li>Home</li>
+						<li>About us</li>
+						<li>Categories</li>
+						<li>info</li>
+						<li>Services</li>
+					</ul>
+
+					<h1>Shop</h1>
+					<ul>
+						<li>Account</li>
+						<li>Help &amp; Guide</li>
+						<li>Shipping & Returns</li>
+						<li>FAQs</li>
+					</ul>
+				</nav> <!-- END .footer-nav -->
+				
+				<address>
+					<h1>Contact</h1>
+					<p>Quotes PSD Template
+					   Rudra Street, 12354 Villa
+					   My State - My Country
+					   987658</p>
+
+					<p>Phone: + 01 234 567 890
+					   Fax: + 01 234 567 890
+					   graphicsfuel@gmail.com</p>
+				</address>
+
+				<h1><span>Subscribe now<span></h1>
+				<p><span>Get our latest news and product updates daily</span></p>
+				<form method="post" action="index.php">
+					<input type="email" name="" placeholder="Email Address">
+					<input type="submit" value="SUBMIT">
+				</form>
+				<div class="sub-footer">
+					<div class="social-media">
+						<ul class="clearfix">
+							<!-- <div class="social-icons clearfix"> -->
+								<li class="facebook left"></li>
+								<li class="twitter left"></li>
+								<li class="linkedin left"></li>
+								<li class="youtube left"></li>
+								<li class="googleplus left"></li>
+								<li class="pinterest left"></li>
+								<li class="instagram left"></li>
+								<li class="email left"></li>
+						<!--	</div> -->
+						</ul>	
+					</div> <!-- END .social-media -->
+
+					<div class="footer-text">
+						<p>2016 &copy; Copyright - Quotes PSD Templates.<br>
+					   	All rights reserved.</p>
+					</div> <!-- END .footer-text -->
+				</div> <!-- END .sub-footer -->
+			</div> <!-- END .row -->	
+		</footer> <!-- END .container-fluid -->
+	</div> <!-- END .wrapper -->
+	<script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
